33742b9afa989905e6ea91fa51965f5608b51921
30padavan10 <pankov.dmitriy@itmh.ru>
2022-05-13 17:48:51 +0500

    less 9

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Movie, Review, Rating
+from .models import Movie, Review, Rating, Actor
 
 
 # сериализаторы нужны чтобы преобразовывать типы данных питон в тип данных json и наоборот
@@ -35,14 +35,30 @@ class RecursiveSerializer(serializers.Serializer):
         return serializer.data
 
 
+class ActorsListSerializer(serializers.ModelSerializer):
+    """Вывод списка актеров и режиссеров"""
+    class Meta:
+        model = Actor
+        fields = ("id", "name", "image")
+
+
+class ActorDetailSerializer(serializers.ModelSerializer):
+    """Вывод данных об актере и режиссере"""
+    class Meta:
+        model = Actor
+        fields = "__all__"
+
+
 class MovieListSerializer(serializers.ModelSerializer):
     """Список фильмов"""
     rating_user = serializers.BooleanField()  # в MovieListView добавили к модели Movie поле rating_user здесь можно
     # его сериализовать вместе с остальными полями
+    #middle_star = serializers.IntegerField() # вариант для целых значений когда Sum делим на Count
+    middle_star = serializers.FloatField()    # вариант для дробных значений когда Avg
 
     class Meta:
         model = Movie
-        fields = ("title", "tagline", "category", "rating_user") # в таком варианте для category отображается id, а не название
+        fields = ("id", "title", "tagline", "category", "rating_user", "middle_star") # в таком варианте для category отображается id, а не название
 
 
 
@@ -70,9 +86,17 @@ class ReviewSerializer(serializers.ModelSerializer):
 class MovieDetailSerializer(serializers.ModelSerializer):
     """Полный фильм"""
     category = serializers.SlugRelatedField(slug_field="name", read_only=True)  # slug_field="name" указывает какое
-    # поле выводить вместо id, read_only=True - поле только для чтения
-    directors = serializers.SlugRelatedField(slug_field="name", read_only=True, many=True)
-    actors = serializers.SlugRelatedField(slug_field="name", read_only=True, many=True)
+    # поле выводить вместо id, оно будет выводиться только одно, read_only=True - поле только для чтения
+
+    # directors = serializers.SlugRelatedField(slug_field="name", read_only=True, many=True)
+    directors = ActorDetailSerializer(read_only=True, many=True)
+    # actors = serializers.SlugRelatedField(slug_field="name", read_only=True, many=True)
+    actors = ActorsListSerializer(read_only=True, many=True)
+    # меняем сериализатор с serializers.SlugRelatedField на ActorDetailSerializer, то выводятся
+    # все данные актеров, если же меняем на ActorsListSerializer, то выводятся поля указанные в ActorsListSerializer
+
+
+
     genres = serializers.SlugRelatedField(slug_field="name", read_only=True, many=True)
     ################# без реализации ниже отзывы не выводятся
     #reviews = ReviewCreateSerializer(many=True)  # без related_name="reviews" в поле movie модели Review отзывы
--- movies/views.py
+++ movies/views.py
@@ -1,34 +1,75 @@
 from django.db import models
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import generics
 
 from .service import get_client_ip
 
-from .models import Movie
-from .serializers import MovieListSerializer, MovieDetailSerializer, ReviewCreateSerializer, CreateRatingSerializer
+from .models import Movie, Actor
+from .serializers import (
+    MovieListSerializer,
+    MovieDetailSerializer,
+    ReviewCreateSerializer,
+    CreateRatingSerializer,
+    ActorsListSerializer,
+    ActorDetailSerializer,
+)
 
 
-class MovieListView(APIView):
-    """Вывод списка фильмов"""
-    # def get(self, request):
-    #     movies = Movie.objects.filter(draft=False)
-    #     serializer = MovieListSerializer(movies, many=True)  # many=True говорит о том что записей несколько
-    #     return Response(serializer.data)
-    # def get до реализации вывода рейтинга
+# class MovieListView(APIView):
+#     """Вывод списка фильмов"""
+#     # def get(self, request):
+#     #     movies = Movie.objects.filter(draft=False)
+#     #     serializer = MovieListSerializer(movies, many=True)  # many=True говорит о том что записей несколько
+#     #     return Response(serializer.data)
+#     # def get до реализации вывода рейтинга
+#
+#     def get(self, request):
+#         """с помощью метода annotate в модель Movie будет добавлено поле с названием rating_user(произвальное),
+#          которое будет Bool значением, если пользователь устанавливал рейтинг то True, иначе False
+#          Case это условное выражение, обрабатываются непосредственно СУБД и внутри себя предполагает:
+#           When - условие,
+#           then - результат при таком условии
+#           default - если условия не выполнились
+#           output_field - тип поля"""
+#         # movies = Movie.objects.filter(draft=False).annotate(                # если в таком варианте добавлять поле
+#         #     rating_user=models.Case(                                        # rating_user, то при наличии рейтингов
+#         #         models.When(ratings__ip=get_client_ip(request), then=True), # от пользователей с другим ip будут
+#         #         default=False,                                              # выводиться дубли записей
+#         #         output_field=models.BooleanField()
+#         #     ),
+#         # )
+#         movies = Movie.objects.filter(draft=False).annotate(
+#             rating_user=models.Count("ratings", filter=models.Q(ratings__ip=get_client_ip(request)))
+#         ).annotate(
+#             #middle_star=models.Sum(models.F('ratings__star')) / models.Count(models.F('ratings'))
+#             middle_star=models.Avg('ratings__star')
+#         )
+#         # для добавление нескольких полей методы annotate можно вызывать друг за другом
+#         # в данном варианте с помощью метода Count считаем количество рейтингов связанных с фильмом и отфильтровываем
+#         # те рейтинги которые не совпадают с нашим ip, так получаем rating_user
+#         # для поля middle_star считаем сумму звезд всех рейтингов, чтобы можно было оперировать результатом как числом
+#         # оборачиваем его функциональным методом F, также Count считает количество рейтингов и также оборачивается F
+#         # полученный цифры делим и получаем среднее значение
+#         # не забыть что добавленные поля нужно также добавить в сериализаторе MovieListSerializer
+#
+#         serializer = MovieListSerializer(movies, many=True)  # many=True говорит о том что записей несколько
+#         return Response(serializer.data)
 
-    def get(self, request):
-        """с помощью метода annotate в модель Movie будет добавлено поле с названием rating_user(произвальное),
-         которое будет Bool значением, если пользователь устанавливал рейтинг то True, иначе False"""
+# Переписываем класс MovieListView с помощью generics класса
+class MovieListView(generics.ListAPIView):
+    """Вывод списка фильмов с помощью generics класса"""
+    serializer_class = MovieListSerializer
+
+    def get_queryset(self):
+        """В данном случае используем метод get_queryset вместо атрибута queryset т.к. нужно добраться до
+        self.request иначе можно было бы спокойно использовать queryset"""
         movies = Movie.objects.filter(draft=False).annotate(
-            rating_user=models.Case(
-                models.When(ratings__ip=get_client_ip(request), then=True),
-                default=False,
-                output_field=models.BooleanField()
-            ),
+            rating_user=models.Count("ratings", filter=models.Q(ratings__ip=get_client_ip(self.request)))
+        ).annotate(
+            middle_star=models.Avg('ratings__star')
         )
-        serializer = MovieListSerializer(movies, many=True)  # many=True говорит о том что записей несколько
-        return Response(serializer.data)
-
+        return movies
 
 class MovieDetailView(APIView):
     """Вывод фильма"""
@@ -57,3 +98,16 @@ class AddStarRatingView(APIView):
             return Response(status=201)
         else:
             return Response(status=400)
+
+
+class ActorsListView(generics.ListAPIView):
+    """Вывод списка Актеров"""
+    queryset = Actor.objects.all()
+    serializer_class = ActorsListSerializer
+
+
+class ActorDetailView(generics.RetrieveAPIView):
+    """Вывод данных об актере/режиссере"""
+    queryset = Actor.objects.all()
+    serializer_class = ActorDetailSerializer
+
